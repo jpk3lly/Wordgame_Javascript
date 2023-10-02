2917556ef14f31968b17315e9661bb85b4c0662d
John-Paul Kelly <jpk3lly@hotmail.com>
2023-10-02 22:35:07 +0100

    fixed scoreboard overflow issues

--- javascript/scoreboard.js
+++ javascript/scoreboard.js
@@ -3,9 +3,9 @@ const boggleHighScores = JSON.parse(localStorage.getItem('boggleHighScores')) ||
 
 boggleHighScoresList.innerHTML = 
 boggleHighScores.map(score => {
-    return `<tr>
-                <td class="high-score">${score.name}</td>
-                <td class="high-score">${score.wordsFound}</td>
-                <td class="high-score">${score.score}</td>
+    return `<tr class="high-score">
+                <td class="individual-score">${score.name}</td>
+                <td class="individual-score">${score.wordsFound}</td>
+                <td class="individual-score">${score.score}</td>
             </tr>`
 }).join("");
