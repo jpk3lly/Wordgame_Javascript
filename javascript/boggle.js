61aceeed3d7ea4e142701ae6450052bcda1305a2
Dhanush <dhanushxenocrate@gmail.com>
2023-10-02 07:27:48 +0000

    Created a text animation for the header when the page load

--- javascript/boggle.js
+++ javascript/boggle.js
@@ -1,6 +1,7 @@
 const newGameBtn = document.getElementById('new-game');
 const scoreboardBtn = document.getElementById('scoreboard');
 const homeButton = document.getElementById('home');
+const textAnimation = document.getElementById("typing-animation")
 
 newGame = () => {
     newGameBtn.addEventListener("click", e => {
@@ -22,6 +23,30 @@ goHome = () => {
     });
 }
 
+startTextAnimation = () => {
+         // JavaScript for the text animation
+         const text = textAnimation.textContent;
+         textAnimation.textContent = ""; // Clear the original text
+         let index = 0;
+         const speed = 100; // Typing speed in milliseconds
+
+         console.log(" Hello ")
+ 
+         function type() {
+             if (index < text.length) {
+                textAnimation.textContent += text.charAt(index);
+                 index++;
+                 setTimeout(type, speed);
+             }
+         }
+ 
+         // Start the typing animation when the page loads
+         window.onload = function() {
+             type();
+         };
+}
+
+startTextAnimation();
 newGame();
 scoreBoard();
 goHome();
